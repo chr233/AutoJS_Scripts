f5b6a39571843963d496509ea2d836114c9085d0
Chr_ <chr@chrxw.com>
2021-05-25 13:15:09 +0800

    适配21年618活动

--- jingdong.js
+++ jingdong.js
@@ -1,56 +1,57 @@
 //JD不定时热更新活动页，请关注脚本更新
 //遇到问题请在发布页留言：
-//https://blog.chrxw.com/archives/2021/01/18/1455.html
-//Ver 1.7 2021.01.21
+//https://blog.chrxw.com/archives/1970/01/01/1570.html
+//Ver 1.1 2021.05.25
 //By Chr_(chr@chrxw.com) 
 //========================= 
-//更新说明
-//重写了任务完成判断方式,防止偶尔报错
-//优化数组访问,应该不会再越界报错了
-//修复若干bug
-//新支持浏览频道任务
-//修复加购和参加会员任务的问题（需要打开开关）
-//修复出现限时任务时报错的问题
+//更新日志
+//重写了判定代码,执行速度更快了
+//修复无法返回的问题
 //========================= 
 //功能配置
-//自动加入会员（设为true启用) 
-let auto_join_vip = false;
-//自动完成加购任务（设为true启用) 
-let auto_add_cart = false;
+//自动加入会员（改成1启用) 
+let auto_join_vip = 0;
+//自动完成加购任务（改成1启用) 
+let auto_add_cart = 0;
 //延时时间倍率（倍数越大延时越久) 
 let sleep_t = 1.0;
 //========================= 
+
 auto.waitFor();
 console.show();
-toast('\n脚本开始运行\n请切换到任务页\nBy Chr_');
+
+toastLog('\n脚本开始运行\n请切换到任务页\nBy Chr_');
 for (let i = 0; i < 10; i++) {
     //等待切换到任务页 
-    if (className("android.view.View").text("我的爆竹").exists() ||
-        className("android.view.View").text("集爆竹").exists()) {
+    if (text("打怪兽分红包").exists() &&
+        text("做任务领金币").exists()) {
         break;
     }
     rsleep(1);
 }
-if (!className("android.view.View").textStartsWith("邀请好友助力").exists()) {
-    advclick(className("android.view.View").text("集爆竹").findOnce());
+if (!textStartsWith("邀请好友助力").exists()) {
+    advclick(text("做任务领金币").findOnce());
 }
-toast("\n即将开始工作\n中途需退出请按【音量+】终止");
+
+toastLog("\n即将开始工作\n中途需退出请按【音量+】终止");
+
 sleep(1000);
+
 for (; ;) {
     let flag = false;
-    let skip = false;
-    let tasks = className("android.view.View").textStartsWith("邀请好友助力").findOnce().parent().parent().children();
-    for (let i = 0; i < tasks.length; i++) {
-        if (tasks[i].childCount() == 5) {
-            tasks[i].child(4).click();
-            rsleep(1);
-            advback();
-            skip = true;
-        }
-    }
-    if (skip) {
-        continue;
-    }
+    // let skip = false;
+    let tasks = textStartsWith("邀请好友助力").findOnce().parent().parent().children();
+    // for (let i = 0; i < tasks.length; i++) { // 限时任务(不清楚还有没有)
+    //     if (tasks[i].childCount() == 5) {
+    //         tasks[i].child(4).click();
+    //         rsleep(1);
+    //         advback();
+    //         skip = true;
+    //     }
+    // }
+    // if (skip) {
+    //     continue;
+    // }
     for (let i = 0; i < tasks.length; i++) {
         if (tasks[i].length < 4) {
             for (let j = 0; j < tasks[i].childCount(); j++) {
@@ -68,33 +69,36 @@ for (; ;) {
             continue;
         }
         let btn = tasks[i].child(3);
+
         log(i, desc);
+
         if (desc.search("8秒") != -1) {
-            if (checks(btn, title)) {
+            if (checks(title)) {
                 advclick(btn);
                 view();
                 flag = true;
             }
-        } else if (desc.search("浏览可得") != -1) {
-            if (checks(btn, title)) {
+        } else if (desc.search("并关注") != -1) {
+            if (checks(title)) {
                 advclick(btn);
-                view2();
+                log('view');
+                view();
                 flag = true;
             }
-        } else if (desc.search("关注频道") != -1) {
-            if (checks(btn, title)) {
+        } else if (desc.search("逛店") != -1) {
+            if (checks(title)) {
                 advclick(btn);
                 view3();
                 flag = true;
             }
-        } else if (auto_join_vip && desc.search("成功入会可得") != -1) {
-            if (checks(btn, title)) {
+        } else if (auto_join_vip && desc.search("成功入会") != -1) {
+            if (checks(title)) {
                 advclick(btn);
                 view3();
                 flag = true;
             }
         } else if (auto_add_cart && desc.search("5个") != -1) {
-            if (checks(btn, title)) {
+            if (checks(title)) {
                 advclick(btn);
                 view4();
                 flag = true;
@@ -106,41 +110,39 @@ for (; ;) {
     }
     if (!flag) {
         toast("\n脚本运行完毕\n模拟浏览任务全部完成（大概）");
+        // console.hide();
         break;
     }
-    console.hide();
 }
 //检查任务是否完成
-function checks(btn, title) {
-    try {
-        return btn.text() == "去完成";
-    } catch (e) {
-        let match = title.match(/(\d+)\/(\d+)/);
-        if (match) {
-            let a = Number(match[1]);
-            let b = Number(match[2]);
-            log(a, b)
-            log(a < b);
-            return a < b;
-        } else {
-            return false;
-        }
+function checks(title) {
+    let match = title.match(/(\d+)\/(\d+)/);
+    if (match) {
+        let a = Number(match[1]);
+        let b = Number(match[2]);
+        // log(a, b, a < b);
+        return a < b;
+    } else {
+        return false;
     }
 }
 //模拟浏览（浏览会场任务） 
 function view() {
     rsleep(3);
-    className("android.widget.Image").text("vk image").findOne(5000);
-    rslide(6);
+    let i = 0;
+    while (!textMatches("获得\\d+金币").exists() && i++ < 8) {
+        if (desc("关闭页面").exists()) {
+            desc("关闭页面").findOnce().click();
+        }
+        rslide(1);
+    }
     rslideR(1);
-    rsleep(3);
-    advback();
+    advback2();
 }
 //模拟浏览（直接返回） 
 function view2() {
-    rsleep(3);
+    rsleep(2);
     back();
-    rsleep(3);
 }
 //模拟浏览（开卡） 
 function view3() {
@@ -148,15 +150,17 @@ function view3() {
     let btn = className("android.view.View").text("确认授权并加入店铺会员").findOne(5000);
     if (btn) {
         advclick(btn);
-        rsleep(2);
+        rsleep(3);
     } else {
+        rslide(1);
         rsleep(1);
     }
     advback();
 }
 //模拟浏览（浏览商品、加购任务） 
 function view4() {
-    rsleep(3);
+    className("android.view.View").textMatches("^¥[0-9]+\.[0-9][0-9]").untilFind();
+
     let add_cart = true;
     let count = 4;
     if (className("android.view.View").text("任意浏览以下5个商品").exists()) {
@@ -170,21 +174,22 @@ function view4() {
     for (let i = 0; i < 5; i++) {
 
         let prices = className("android.view.View").textMatches("^¥[0-9]+\.[0-9][0-9]").untilFind();
+
         log(i, prices[i].text());
-        let good = prices[i].parent().parent();
+
+        let good = prices[i].parent();
         if (good.childCount() != count) {
             log('已完成，跳过');
             continue;
         }
         if (!add_cart) {
             advclick(good);
-            rsleep(2);
+            rsleep(1);
             rslide(1);
             className("android.widget.ImageView").desc("返回").click();
         } else {
             good.child(4).click();
-            rsleep(2);
-            rslide(1);
+            rsleep(1);
             className("android.widget.ImageView").desc("返回").click();
         } rslide(1);
         rsleep(2);
@@ -201,7 +206,7 @@ function rsleep(s) {
 }
 //模拟返回 
 function advback() {
-    if (className("android.view.View").text("集爆竹").exists()) {
+    if (text("做任务领金币").exists()) {
         return;
     }
     for (let i = 0; i < 3; i++) {
@@ -211,8 +216,28 @@ function advback() {
             continue;
         }
         className("android.widget.ImageView").desc("返回").click();
+        rsleep(3);
+        if (text("做任务领金币").exists()) {
+            return;
+        }
+    }
+    log("定位不到返回按钮，模拟返回键");
+    back();
+}
+//模拟返回 
+function advback2() {
+    if (text("做任务领金币").exists()) {
+        return;
+    }
+    for (let i = 0; i < 3; i++) {
+        if (!textMatches("获得\\d+金币").exists()) {
+            rslideR(1);
+            rsleep(2);
+            continue;
+        }
+        advclick(textMatches("获得\\d+金币").findOnce());
         rsleep(5);
-        if (className("android.view.View").text("集爆竹").exists()) {
+        if (text("做任务领金币").exists()) {
             return;
         }
     }
@@ -245,12 +270,4 @@ function rslideR(i) {
 function advclick(uiobject) {
     let rect = uiobject.bounds();
     click(rect.centerX(), rect.centerY());
-}
-function test() {
-    let tasks = className("android.view.View").textStartsWith("邀请好友助力").findOnce().parent().parent().children();
-    for (let i = 0; i < tasks.length; i++) {
-        for (let j = 0; j < tasks[i].childCount(); j++) {
-            log(tasks[i].child(j).text());
-        }
-    }
 }
